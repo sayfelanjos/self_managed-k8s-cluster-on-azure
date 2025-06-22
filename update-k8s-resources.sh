a71243d51f618945d6d20687201e689867001677
saymon <sayfelanjos@gmail.com>
2025-06-22 19:07:09 -0300

    Update Terraform version to v1.11.3

---
+++ update-k8s-resources.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  "https://api.github.com/repos/sayfelanjos/self_managed-k8s-cluster-on-azure /dispatches" \
+  -d '{"event_type": "update-resources"}'
